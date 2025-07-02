739fb15e3f69b70f53d94f0eb5e353628e690472
Rishabh verma <mail.rishabhverma@gmail.com>
2025-07-02 16:58:32 +0530

    fix: sidebar layout error

--- src/core/layouts/SidebarLayout.tsx
+++ src/core/layouts/SidebarLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from '@tanstack/react-router'
 
-import AppSidebar from '@/shared/components/AppSidebar'
 import { SidebarProvider } from '@/shared/ui/sidebar'
 
 function SidebarLayout() {
   return (
     <SidebarProvider>
-      <AppSidebar />
       <div className="w-full">
         <nav></nav>
         <main>
