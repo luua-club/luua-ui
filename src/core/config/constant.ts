da799f2e64fec59ea90e7bdc96753574a812cb75
Rishabh verma <mail.rishabhverma@gmail.com>
2025-07-07 19:44:02 +0530

    Feature/chat box (#11)
    * feat: added prompt box
    
    * chore: added bottom buttons for start new flow

--- src/core/config/constant.ts
+++ src/core/config/constant.ts
@@ -4,3 +4,12 @@
  * @default 'luua-user'
  */
 export const LUUA_USER_KEY = import.meta.env.LUUA_USER_KEY || 'luua-user'
+
+/**
+ * Suggested text that is shown inside prompt box
+ */
+export const SUGGESTED_PROMPT_TEXT = [
+  'Start with a prompt: Announce our weekend sale...',
+  'Or paste a link to a tweet, or linkedin post you love',
+  'Let Luua handle the rest...',
+]
