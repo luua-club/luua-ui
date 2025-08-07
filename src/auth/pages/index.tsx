ecd711356c2362699b613d096746589ee70f9548
Rishabh verma <mail.rishabhverma@gmail.com>
2025-08-08 00:30:27 +0530

    fix: social disconnect bug, fetch profile not happening

--- src/auth/pages/index.tsx
+++ src/auth/pages/index.tsx
@@ -1,11 +1,12 @@
 import { CredentialResponse } from '@react-oauth/google'
+import { useQueryClient } from '@tanstack/react-query'
 import { useRouter } from '@tanstack/react-router'
 import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 
 import { authApi } from '@/core/api/auth.api'
 import { userApi } from '@/core/api/user.api'
-import { LUUA_USER_KEY } from '@/core/config/constant'
+import { LUUA_USER_KEY, QUERY_KEYS } from '@/core/config/constant'
 import { useAppDispatch } from '@/core/hooks/global-state.hook'
 import { ILoginResponse } from '@/core/models/auth.model'
 import { cn } from '@/shared/utils'
@@ -25,6 +26,7 @@ function Login() {
   // UseHooks
   const router = useRouter()
   const dispatch = useAppDispatch()
+  const queryClient = useQueryClient()
 
   // Constants
   const key = LUUA_USER_KEY
@@ -73,14 +75,19 @@ function Login() {
     setLocalStorageItem(key, res)
 
     try {
-      const response = await userApi.getUser()
+      const response = await queryClient.fetchQuery({
+        queryKey: [QUERY_KEYS.user],
+        queryFn: () => userApi.getUser(),
+        staleTime: 0,
+      })
+
       dispatch(setUser(response.data))
       setLocalStorageItem(key, {
         ...getLocalStorageItem<ILoginResponse>(key),
         user: response.data,
       })
+
       router.navigate({ to: '/dashboard' })
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (err) {
       toast.error('Something went wrong, Not able to login, Please try again !')
     } finally {
