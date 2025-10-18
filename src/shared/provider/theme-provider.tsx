25f09de940d02cfebb0d08904a96f60ed5643e7c
Shubham Pathak <shubham.pathak@zomato.com>
2025-10-18 22:04:13 +0530

    Default theme to light

--- src/shared/provider/theme-provider.tsx
+++ src/shared/provider/theme-provider.tsx
@@ -24,7 +24,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 
 export function ThemeProvider({
   children,
-  defaultTheme = 'system',
+  defaultTheme = 'light',
   storageKey = THEME_LOCAL_STORAGE_KEY,
   ...props
 }: ThemeProviderProps) {
@@ -36,17 +36,6 @@ export function ThemeProvider({
     const root = window.document.documentElement
 
     root.classList.remove('light', 'dark')
-
-    if (theme === 'system') {
-      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
-        .matches
-        ? 'dark'
-        : 'light'
-
-      root.classList.add(systemTheme)
-      return
-    }
-
     root.classList.add(theme)
   }, [theme])
 
